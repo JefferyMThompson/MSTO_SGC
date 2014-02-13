b987c08cf37a3b56e539c45f743578e9cfc848ee
Jeffery M. Thompson <thompj9@rpi.edu>
2014-02-13 16:27:43 -0500

    Adding utilities directory and astro_coordinates.py files. Creating stubs for the redenning correction

---
+++ utilities/astro_coordinates.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# This file contains functions for use with scripts that process data files
+# from the SDSS Data Release 9.
+# Originally generated 2/13/2014
+# Written by Jeffery M. Thompson
+# Including code from Matthew Newby
+# The Newby Utilities can be found at:
+# https://github.com/MNewby/Newby-tools
+
+# import modules
+
+from astropy import Distance
+from astropy import ICRS
+import astropy.units as u
+
+
